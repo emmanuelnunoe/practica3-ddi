0c9adbda60ef0510a3a42b61929684b04a8219b0
Emmanuel <kehkhr@gmail.com>
2019-03-07 21:38:38 -0800

    working rigidbody and colliders
    colliders and rigidbody working!

---
+++ colliders/Assets/Interactable.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour 
+{
+	public Color newColor;
+	public GameObject infoCanvas;
+	Renderer rend;
+	bool isInsideZone;
+
+	/// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	protected virtual void Awake()
+	{
+		rend = GetComponent<Renderer>();
+	}
+
+	/// <summary>
+	/// Start is called on the frame when a script is enabled just before
+	/// any of the Update methods is called the first time.
+	/// </summary>
+	void Start()
+	{
+		infoCanvas.SetActive(false);
+	}
+
+	/// <summary>
+	/// Update is called every frame, if the MonoBehaviour is enabled.
+	/// </summary>
+	void Update()
+	{
+		if(isInsideZone)
+		{
+			if(Input.GetKeyDown(KeyCode.C))
+			{
+				Interact();
+			}
+		}
+	}
+
+	/// <summary>
+	/// OnTriggerEnter is called when the Collider other enters the trigger.
+	/// </summary>
+	/// <param name="other">The other Collider involved in this collision.</param>
+	void OnTriggerEnter(Collider other)
+	{
+		if(!other.CompareTag("Player"))
+			return;
+		// Debug.Log("Entraste en la esfera!");
+		// rend.material.color = newColor;
+		isInsideZone = true;
+		infoCanvas.SetActive(true);
+	}
+
+	/// <summary>
+	/// OnTriggerExit is called when the Collider other has stopped touching the trigger.
+	/// </summary>
+	/// <param name="other">The other Collider involved in this collision.</param>
+	void OnTriggerExit(Collider other)
+	{
+		infoCanvas.SetActive(false);
+		isInsideZone = false;
+	}
+
+	public virtual void Interact()
+	{
+		rend.material.color = newColor;
+	}
+}
---
+++ colliders/Assets/sphereBall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereBall : Interactable 
+{
+	public float kickForce = 30f;
+	public Vector3 kickDirection;
+	Rigidbody rb;
+
+	/// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	protected override void Awake()
+	{
+		base.Awake();
+		rb = GetComponent<Rigidbody>();
+	}
+	public override void Interact()
+	{
+		Debug.Log("SphereBall");
+		rb.AddForce(kickDirection * kickForce, ForceMode.Force);
+	}
+}
